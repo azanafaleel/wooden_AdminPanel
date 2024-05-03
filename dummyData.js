804601aeb225a825ef56872f7dd5157d04306f68
Azana <117095482+azanafaleel@users.noreply.github.com>
2024-05-03 05:40:38 -0700

    Add files via upload

---
+++ dummyData.js
@@ -0,0 +1,235 @@
+const userData = [
+  {
+    name: "Jan",
+    "Active User": 4000,
+  },
+  {
+    name: "Feb",
+    "Active User": 3000,
+  },
+  {
+    name: "Mar",
+    "Active User": 2000,
+  },
+  {
+    name: "Jun",
+    "Active User": 2780,
+  },
+  {
+    name: "Jul",
+    "Active User": 1890,
+  },
+  {
+    name: "Aug",
+    "Active User": 2390,
+  },
+  {
+    name: "Sep",
+    "Active User": 3490,
+  },
+  {
+    name: "Oct",
+    "Active User": 1024,
+  },
+  {
+    name: "Nov",
+    "Active User": 4500,
+  },
+  {
+    name: "Dec",
+    "Active User": 3490,
+  },
+];
+
+const productSales = [
+  {
+    name: "Jan",
+    sales: 4000,
+  },
+  {
+    name: "Feb",
+    sales: 3000,
+  },
+  {
+    name: "Mar",
+    sales: 2000,
+  },
+];
+
+const userDetails = [
+  {
+    id: 1,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 2,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 3,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 4,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 5,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 6,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 7,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 8,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 9,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 10,
+    username: "Snow",
+    avatar:
+      "https://i.pinimg.com/236x/b8/de/16/b8de166ec98456019db9c10fbcda436b.jpg",
+    email: "wow@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+];
+
+const productDataJson = [
+  {
+    id: 1,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 2,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 3,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 4,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 5,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 6,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 7,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 8,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 9,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 10,
+    name: "chair",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCqZPB0FJu4dOK9NYO7DVQ68SPwB5e7j8Uw&usqp=CAU",
+    stock: "10",
+    status: "active",
+    price: "$120.00",
+  },
+];
+
+export { userData, userDetails, productDataJson, productSales };
